71d587e7d79b08785a7ee9436b3b35c09443fbae
stack72 <public@paulstack.co.uk>
2015-08-07 12:38:35 +0100

    Updating the producer to take command line arguments for kafka-nodes, deploymentType (start/stop), deploymentMessage and application. Also removed the consumer as we no longer require it

--- producer.py
+++ producer.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python
-import threading, logging, time, sys
+import threading, logging, time, sys, getopt
 import traceback
+import datetime
 
-import kafka
-from kafka.client import KafkaClient
-from kafka.producer import SimpleProducer
+from kafka import SimpleProducer, KafkaClient
 
 class Producer():
 
-    def run(self):
-        try:
-            client = KafkaClient("pp-logkafka-01.qasql.opentable.com:9092,pp-logkafka-02.qasql.opentable.com:9092,pp-logkafka-03.qasql.opentable.com:9092")
+    def __init__(self, nodes=None, application=None, depType=None, message=None):
+        global kafkaNodes
+        kafkaNodes = nodes
+        global applicationName
+        applicationName = application
+        global deploymentType
+        deploymentType = depType
+        global deploymentMessage
+        deploymentMessage = message
 
-            producer = SimpleProducer(client, "")
-            producer.send_messages("lifecycle_events","test")
+    def run(self):
+        current_date = datetime.datetime.now()
+
+        try:
+            kafka = KafkaClient(kafkaNodes)
+
+            producer = SimpleProducer(kafka, async=True)
+            producer.send_messages('logs',applicationName,deploymentType,deploymentMessage,current_date.isoformat())
 
         except:
             tb = traceback.format_exc()
@@ -21,4 +32,25 @@ class Producer():
             print tb
 
 if __name__ == "__main__":
-    Producer().run()
+    kafkaNodes = None;
+    applicationName = None;
+    deploymentType = None;
+    deploymentMessage = None;
+
+    myopts, args = getopt.getopt(sys.argv[1:],"n:a:d:m:")
+    ###############################
+    # o == option
+    # a == argument passed to the o
+    ###############################
+    for o, a in myopts:
+        if o == '-n':
+            kafkaNodes = a
+        elif o == '-a':
+            applicationName = a
+        elif o == '-d':
+            deploymentType = a
+        elif o == '-m':
+            deploymentMessage = a
+        else:
+            print("Usage: %s -n kafka-nodes-list -a applicationName -d deploymentType -m deploymentMessage" % sys.argv[0])
+    producer = Producer(kafkaNodes, applicationName, deploymentType, deploymentMessage).run()
